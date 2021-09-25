ccc792e52da955bbf46457032178428504f7356f
amitha-vikraman <90146843+amitha-vikraman@users.noreply.github.com>
2021-09-26 00:20:00 +0530

    Added Gute Nacht

--- PrintGoodNight.java
+++ PrintGoodNight.java
@@ -3,6 +3,7 @@ public class PrintGoodNight {
 	public static void main(String[] args) {
 		System.out.println("Good night"); 
 		System.out.println("Bonne nuit"); 
+                System.out.println("Gute Nacht");
 	}
 
 }
